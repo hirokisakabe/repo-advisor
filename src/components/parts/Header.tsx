5e22df8a995900c907884f1ee892c16e86664d59
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2023-09-19 13:47:32 +0900

    ヘッダーをリンクにする

--- src/components/parts/Header.tsx
+++ src/components/parts/Header.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import { Typography } from "../ui";
 
 export function Header() {
   return (
     <div className="flex pt-2">
       <div className="w-full">
-        <Typography size="text-xl">Repo Advisor</Typography>
+        <Link href="/dashboard">
+          <Typography size="text-xl">Repo Advisor</Typography>
+        </Link>
       </div>
     </div>
   );
