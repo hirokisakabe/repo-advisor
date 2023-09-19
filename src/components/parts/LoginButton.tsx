f82e7caef61e0c87736335d3b0136a2cea7ceca2
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2023-09-19 16:45:49 +0900

    LoginButtonでGitHubでログインすることを明示

--- src/components/parts/LoginButton.tsx
+++ src/components/parts/LoginButton.tsx
@@ -6,7 +6,7 @@ import { signIn } from "next-auth/react";
 export function LoginButton() {
   return (
     <Button variant="secondary" onClick={() => signIn()}>
-      Sign in
+      Sign in with GitHub
     </Button>
   );
 }
