13f548e3f826b3db017f6bc79e6e7330ccbf6190
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2023-10-10 15:52:59 +0900

    wip

--- src/lib/server/fetchFromOpenAIChatCompletions.ts
+++ src/lib/server/fetchFromOpenAIChatCompletions.ts
@@ -11,17 +11,23 @@ export async function fetchFromOpenAIChatCompletions({
 }): Promise<Result<any, string>> {
   console.log("content", content);
 
-  const completion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "user",
-        content,
-      },
-    ],
-    model: "gpt-3.5-turbo",
-  });
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content,
+        },
+      ],
+      model: "gpt-3.5-turbo",
+    });
 
-  console.log("completion", JSON.stringify(completion));
+    console.log("completion", JSON.stringify(completion));
 
-  return Result.success(completion.choices[0].message.content);
+    return Result.success(completion.choices[0].message.content);
+  } catch (error) {
+    console.error(error);
+
+    return Result.failure(JSON.stringify(error));
+  }
 }
